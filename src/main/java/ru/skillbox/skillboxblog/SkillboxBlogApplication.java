d0fc6ef0fe8f165de9842be77b233636b936a4df
Morozov Aleksey <a.m.morozov@gmail.com>
2021-08-21 18:48:25 +0300

    start point of project

---
+++ src/main/java/ru/skillbox/skillboxblog/SkillboxBlogApplication.java
@@ -0,0 +1,13 @@
+package ru.skillbox.skillboxblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillboxBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkillboxBlogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/skillbox/skillboxblog/SkillboxBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.skillbox.skillboxblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkillboxBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
